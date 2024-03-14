b7086e81a2a869dfe3f44cc5a1ee602d753aa39f
Mathis <yidhra@tuta.io>
2024-03-14 16:46:30 +0100

    update class index

--- src/game/class/index.js
+++ src/game/class/index.js
@@ -1,2 +1,9 @@
 import { GameView } from "./game-view";
 import { TicTacToe } from "./tic-tac-toe";
+import { ThemeProvider } from "./theme-provider.js";
+
+export default Class = {
+  GameView,
+  TicTacToe,
+  ThemeProvider
+}
