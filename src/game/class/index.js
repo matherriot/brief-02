768ce2efa055b9df3b50ebb1c62ee8333231bf0c
Mathis <yidhra@tuta.io>
2024-03-15 17:01:58 +0100

    update class index

--- src/game/class/index.js
+++ src/game/class/index.js
@@ -1,11 +1,17 @@
-import { GameView } from "./game-view";
-import { TicTacToe } from "./tic-tac-toe";
+import { GameView } from "./game-view.js";
+import { TicTacToe } from "./tic-tac-toe.js";
 import { ThemeProvider } from "./theme-provider.js";
+import { IndexedDb } from "./indexed-db-handler.js";
+
+const Data = {
+  IndexedDb
+}
 
 const Class = {
   GameView,
   TicTacToe,
-  ThemeProvider
+  ThemeProvider,
+  Data,
 }
 
 export default Class;
